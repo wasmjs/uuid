f60eb5afb3e8c0c66448cc10a9552f933579eb79
Amaury Martiny <amaury.martiny@protonmail.com>
2019-03-16 23:27:23 +0100

    test: add test for v3

--- tests/web.rs
+++ tests/web.rs
@@ -3,11 +3,20 @@
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
+extern crate wasmjs_uuid;
+
 use wasm_bindgen_test::*;
+use wasmjs_uuid::v3::uuidv3;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
+const MY_NAMESPACE: &str = "1b671a64-40d5-491e-99b0-da01ff1f3341";
+
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+fn uuidv3_should_work() {
+	// https://github.com/kelektiv/node-uuid#version-3
+	assert_eq!(
+		uuidv3("hello world", MY_NAMESPACE),
+		"042ffd34-d989-321c-ad06-f60826172424"
+	);
 }
